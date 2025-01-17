c1e230bf3fb7083e614a93b440e6b52bd2a8eb8a
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-01-17 20:30:04 +0330

    style (src/components/header/NavSearchBox): Adjust width for different screens, change placeholder text

--- src/components/header/NavSearchBox.jsx
+++ src/components/header/NavSearchBox.jsx
@@ -7,14 +7,14 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
  */
 export default function NavSearchBox() {
   return (
-    <form className="bg-red-300 h-12 w-96 rounded-full relative ">
+    <form className="h-12 w-80 lg:w-96 rounded-full relative hidden md:block">
       <button className="h-10 w-10 rounded-full bg-[#f1da69] text-slate-900 absolute top-1 left-1 ">
         <FontAwesomeIcon icon={faSearch} />
       </button>
       <input
         type="text"
         id=""
-        placeholder="Medicite and healthcare items"
+        placeholder="Healthcare items"
         className="w-full pl-[52px] h-full rounded-full bg-white text-lg"
       />
     </form>
