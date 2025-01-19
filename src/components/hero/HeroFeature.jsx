5537e00f6bd99e8a067a9519885411390563241f
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-01-19 22:23:42 +0330

    docs (src/components/hero/HeroFeature): Document HeroFeature component

--- src/components/hero/HeroFeature.jsx
+++ src/components/hero/HeroFeature.jsx
@@ -1,4 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+/**
+ * HeroFeature Component
+ *
+ * Displays a feature item with an icon and a description.
+ * Props:
+ * - `description` (string): The text description of the feature.
+ * - `icon` (object): The icon to be displayed.
+ * - `bgColor` (string): The background color class for the icon.
+ */
 export default function HeroFeature({ description, icon, bgColor }) {
   return (
     <div className="flex justify-start items-center gap-2 lg:gap-4 max-w-40 md:max-w-72 bg-gray- text-sky-100">
