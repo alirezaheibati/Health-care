a1ad59d3229687ace131c2295c6cc1817c9fc08e
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-01-25 22:56:47 +0330

    docs (src/components/user-review): Document ReviewContainer component

--- src/components/user-review/ReviewContainer.jsx
+++ src/components/user-review/ReviewContainer.jsx
@@ -1,5 +1,12 @@
 import ReviewsBox from "./ReviewsBox.jsx";
 import ReviewsAverage from "./ReviewsAverage.jsx";
+/**
+ * ReviewContainer Component
+ *
+ * Displays a section containing the average reviews rating and a list of user reviews. \
+ *
+ * Props: None
+ */
 export default function ReviewContainer() {
   return (
     <section className="py-8 text-[#252c62]">
