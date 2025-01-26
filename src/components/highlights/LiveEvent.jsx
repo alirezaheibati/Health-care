08a0496f0e10c17cf33b0238b02ddf51f1ad19c6
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-01-26 23:09:32 +0330

    feat (src/components/highlights): Create LiveEvent component

---
+++ src/components/highlights/LiveEvent.jsx
@@ -0,0 +1,23 @@
+import VideoCall from "./../../assets/video-call.png";
+
+export default function LiveEvent() {
+  return (
+    <div className="w-full h-full relative bg-[#252c62] text-slate-50 flex flex-col justify-between items-start rounded-xl p-3 sm:p-8">
+      <img
+        src={VideoCall}
+        alt="Video Call"
+        className="absolute h-4/5 right-2 top-2 opacity-30"
+      />
+      <p className="bg-slate-50 px-2 w-28 mb-2 sm:mb-4 rounded-md text-[#252c62]">
+        <span className="bg-red-400 w-2 h-2 inline-block rounded-full"></span>{" "}
+        Live event
+      </p>
+      <div>
+        <h2 className="text-2xl sm:text-3xl font-semibold z-10 relative">
+          Healty Habits for a<br /> Happy Heart
+        </h2>
+        <p className="text-sm">April 9, 2025 08:00 PM</p>
+      </div>
+    </div>
+  );
+}
