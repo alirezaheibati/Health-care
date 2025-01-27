0becba66bda28c05def28276f3447ea8e3651833
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-01-27 19:49:08 +0330

    feat (src/components/footer): Create Footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,33 @@
+import googlePlay from "./../../assets/googlePlay.png";
+import appStore from "./../../assets/appStore.png";
+import {
+  faInstagram,
+  faLinkedinIn,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import SocialMedaiBtn from "./SocialMedaiBtn";
+
+export default function Footer() {
+  return (
+    <div className="rounded-t-2xl bg-[#252c62] p-12 pb-0 ">
+      <div className="bg-slate-50 rounded-t-2xl p-8 pb-2 flex justify-between items-center">
+        <div className="flex justify-start items-center gap-2 [&_img]:w-40">
+          <a href="#">
+            <img src={googlePlay} alt="Google Play" />
+          </a>
+          <a href="#">
+            <img src={appStore} alt="App store" />
+          </a>
+        </div>
+        <div className="flex justify-start items-center gap-2">
+          <SocialMedaiBtn icon={faInstagram} linkTo={"#"} />
+          <SocialMedaiBtn icon={faTwitter} linkTo={"#"} />
+          <SocialMedaiBtn icon={faLinkedinIn} linkTo={"#"} />
+        </div>
+      </div>
+      <p className="bg-slate-50 text-center text-[#252c62] pb-2">
+        Copyright@2025 - Designed by Love. Alirezazzz
+      </p>
+    </div>
+  );
+}
