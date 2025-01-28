487f6a2a4c6f5403d318e9b46be423d5952386b9
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-01-28 23:11:16 +0330

    feat (src/components/user-review): Update ReviewsBox component to fetch reviews from API and handle pagination

--- src/components/user-review/ReviewsBox.jsx
+++ src/components/user-review/ReviewsBox.jsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import UserReview from "./UserReview";
+import PaginationDot from "./PaginationDot";
 /**
  * ReviewsBox Component
  *
@@ -7,20 +9,55 @@ import UserReview from "./UserReview";
  * Props: None
  */
 export default function ReviewsBox() {
+  const [reviews, setReviews] = useState([]);
+  const [activeReview, setActiveReview] = useState(null);
+  function handleSelectionDot(id) {
+    setActiveReview(id);
+  }
+  useEffect(() => {
+    async function loadReviews() {
+      try {
+        const responese = await fetch(
+          "https://parseapi.back4app.com/classes/reviews",
+          {
+            headers: {
+              "X-Parse-Application-Id":
+                "Iz7O9D835Yq51vOgGfffJceqQ2Fm9hri2JwVyDGu",
+              "X-Parse-REST-API-Key":
+                "49U8AGgLP9GzHHz3vGnj2bRVJv256evN5OG9WgeN",
+            },
+          }
+        );
+        if (!responese.ok) {
+          throw new Error("Fetching reviews failed");
+        }
+        const data = await responese.json();
+        setReviews(data.results);
+        setActiveReview(data.results[0].objectId);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    loadReviews();
+  }, []);
   return (
     <>
-      <UserReview />
+      {reviews.map((review) => (
+        <UserReview
+          key={review.patient}
+          review={review}
+          isActive={review.objectId === activeReview}
+        />
+      ))}
       <div className="rounded-full bg-gray-100 h-10 w-52 mx-auto mt-4 flex justify-center items-center gap-2">
-        <div className="rounded-full w-2 h-2 bg-gray-300"></div>
-        <div className="rounded-full w-2 h-2 bg-gray-300"></div>
-        <div className="rounded-full w-2 h-2 bg-gray-300"></div>
-        <div className="rounded-full w-2 h-2 bg-gray-300"></div>
-        <div className="rounded-full w-2 h-2 bg-gray-600"></div>
-        <div className="rounded-full w-2 h-2 bg-gray-300"></div>
-        <div className="rounded-full w-2 h-2 bg-gray-300"></div>
-        <div className="rounded-full w-2 h-2 bg-gray-300"></div>
-        <div className="rounded-full w-2 h-2 bg-gray-300"></div>
-        <div className="rounded-full w-2 h-2 bg-gray-300"></div>
+        {reviews.map((review) => (
+          <PaginationDot
+            key={review.objectId}
+            id={review.objectId}
+            isActive={review.objectId === activeReview}
+            onSelectDot={handleSelectionDot}
+          />
+        ))}
       </div>
     </>
   );
