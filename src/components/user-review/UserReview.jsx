fc617cc97e3187759393d5dc8cd21381a5a52920
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-01-28 23:04:53 +0330

    feat (src/components/user-review): Update UserReview component to handle active state and render based on review data

--- src/components/user-review/UserReview.jsx
+++ src/components/user-review/UserReview.jsx
@@ -1,4 +1,3 @@
-import patient from "./../../assets/doctors/doctor-8.png";
 /**
  * UserReview Component
  *
@@ -6,25 +5,27 @@ import patient from "./../../assets/doctors/doctor-8.png";
  *
  * Props: None
  */
-export default function UserReview() {
+export default function UserReview({ review, isActive }) {
   return (
-    <div className="border-2 rounded-lg p-2 lg:p-12 w-3/4 mx-auto flex justify-between items-center flex-col lg:flex-row">
-      <div className="flex gap-3 justify-start items-center py-4 lg:p-0">
+    <div
+      className={`border-2 rounded-lg p-2 lg:p-12 w-3/4 mx-auto ${
+        isActive ? "flex" : "hidden"
+      } justify-between items-center flex-col lg:flex-row`}
+    >
+      <div className="flex gap-3 justify-start items-center py-4 lg:p-0 lg:w-60">
         <img
-          src={patient}
-          alt="patient"
-          className="w-16 h-16 rounded-full bg-red-300"
+          src={review.image}
+          alt={review.patient}
+          className="w-16 h-16 rounded-full"
         />
         <div>
-          <p>Alireza heibati</p>
-          <p className="text-sm text-gray-500">patient</p>
+          <p>{review.patient}</p>
+          <p className="text-sm text-gray-500 ">patient</p>
         </div>
       </div>
       <div className="h-[2px] w-full lg:h-16 lg:w-[2px] bg-gray-300"></div>
-      <div className="lg:w-3/5 py-4 lg:p-0">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt doloribus
-        alias ut optio sint, nemo fuga ex tenetur officia iure sit dignissimos
-        quia, nesciunt laborum, eveniet deleniti natus suscipit necessitatibus.
+      <div className="lg:w-3/5 py-4 lg:p-0 lg:line-clamp-2 line-clamp-none">
+        {review.comment}
       </div>
     </div>
   );
