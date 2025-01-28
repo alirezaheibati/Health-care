9e2621836847bb577f60a87abdbad91e9ca3dd9c
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-01-28 23:01:38 +0330

    docs (src/components/user-review): Document PaginationDot component

--- src/components/user-review/PaginationDot.jsx
+++ src/components/user-review/PaginationDot.jsx
@@ -1,3 +1,16 @@
+/**
+ * PaginationDot Component
+ *
+ * Displays a clickable dot for pagination, indicating the current active user review.
+ *
+ * Props:
+ * - `id` (number): The unique identifier for the pagination dot.
+ * - `isActive` (boolean): Indicates whether the dot is active or not.
+ * - `onSelectDot` (function): Callback function to handle the dot selection.
+ *
+ * Methods:
+ * - `handleClick`: Handles the click event and calls the `onSelectDot` function with the dot's `id`.
+ */
 export default function PaginationDot({ id, isActive, onSelectDot }) {
   function handleClick() {
     onSelectDot(id);
