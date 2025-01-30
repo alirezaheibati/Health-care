aeea0407b87554cef6fd104920b1eb5d4578efcc
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-01-30 19:12:15 +0330

    feat (src/components/ui/Spinner): Create  Spinner component to display a loading spinner

---
+++ src/components/ui/Spinner.jsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className="relative w-full h-full p-8 flex justify-center items-center z-50">
+      <div className="border-x-orange-500 border-[12px] border-transparent w-40 h-40 rounded-full animate-spin"></div>
+    </div>
+  );
+}
