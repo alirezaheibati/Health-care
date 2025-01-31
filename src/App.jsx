ee23897a37e9d0c9475fa988b5744f72e0650bbf
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-01-31 19:52:49 +0330

    feat (src/App.jsx): Set up routing in App component with a single route for the Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+const router = createBrowserRouter([
+  {
+    path: "projects/healthcare",
+    element: <Home />,
+  },
+]);
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
