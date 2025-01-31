f0580a28a8cff45d953e0c81023d3145a9132e8c
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-01-31 19:57:22 +0330

    feat (src/store/ui-slice): Create ui- slice to handle spinner visibility state

---
+++ src/store/ui-slice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const uiSlice = createSlice({
+  name: "ui",
+  initialState: { spinnerVisibility: false },
+  reducers: {
+    showSpinner(state) {
+      state.spinnerVisibility = true;
+    },
+    hideSpinner() {
+      state.spinnerVisibility = false;
+    },
+  },
+});
+
+export const uiActions = uiSlice.actions;
+export default uiSlice;
