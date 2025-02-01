404c202a9004d3038402cfd06d289c93e1170a00
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-02-01 23:50:30 +0330

    docs (src/pages/RootLayout): Document RootLayout component

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,5 +1,12 @@
 import { Outlet } from "react-router-dom";
 import TopNav from "../components/header/TopNav";
+/**
+ * RootLayout Component
+ *
+ * Provides a consistent layout with a top navigation bar and a placeholder for nested routes.
+ * This component ensures that the `TopNav` component is always visible while displaying
+ * different content below it based on the current route.
+ */
 export default function RootLayout() {
   return (
     <>
