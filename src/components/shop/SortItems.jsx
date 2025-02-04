c02d99ec6adba76dbf6cc512afb9614d6ce20ec0
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-02-04 20:02:57 +0330

    style (src/components/shop/SortItems): Add responsive styles to the select element and filter button

--- src/components/shop/SortItems.jsx
+++ src/components/shop/SortItems.jsx
@@ -3,13 +3,14 @@ import { faFilter } from "@fortawesome/free-solid-svg-icons";
 export default function SortItems() {
   return (
     <div className="flex justify-start gap-2 items-center">
-      <select className="h-10 rounded-xl outline-none grow border px-1">
+      <select className="h-10 rounded-xl outline-none grow sm:grow-0 sm:w-80 border px-1">
         <option>most viewed</option>
         <option>Price-Asc</option>
         <option>Price-Dec</option>
         <option>most bought</option>
       </select>
-      <button className="bg-white w-10 h-10 rounded-xl border">
+      <button className="bg-white w-10 h-10 sm:w-auto sm:px-4 rounded-xl border md:hidden">
+        <span className="hidden sm:inline mr-2">Filter</span>
         <FontAwesomeIcon icon={faFilter} />
       </button>
     </div>
