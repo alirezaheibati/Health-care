94578a020890573e941cb9e305cba6f43ad91bdf
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-02-05 17:53:24 +0330

    feat (src/store/index.js): Integrate items slice into the Redux store configuration

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import uiSlice from "./ui-slice";
+import itemsSlice from "./items-slice";
 const store = configureStore({
   reducer: {
     ui: uiSlice.reducer,
+    items: itemsSlice.reducer,
   },
 });
 export default store;
