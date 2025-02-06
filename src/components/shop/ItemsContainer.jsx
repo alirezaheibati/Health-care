618ebd6d464674879014fa415bbe2539b93b2a7e
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-02-06 18:13:33 +0330

    feat (src/components/shop/ItemsContainer): Update ItemsContainer component to map and display ShopItem components based on Redux state

--- src/components/shop/ItemsContainer.jsx
+++ src/components/shop/ItemsContainer.jsx
@@ -1,13 +1,13 @@
 import ShopItem from "./ShopItem";
+import { useSelector } from "react-redux";
 
 export default function ItemsContainer() {
+  const items = useSelector((state) => state.items.items);
   return (
     <div className="p-4 flex justify-center sm:justify-between items-stretch flex-wrap gap-2 w-full md:w-2/3 lg:w-3/4 bg-green-500">
-      <ShopItem />
-      <ShopItem />
-      <ShopItem />
-      <ShopItem />
-      <ShopItem />
+      {items.map((item) => (
+        <ShopItem key={item.objectId} item={item} />
+      ))}
     </div>
   );
 }
