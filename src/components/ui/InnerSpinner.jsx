ea284972928c99a3d7e8677c4fba4966b0f756f4
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-02-06 18:10:02 +0330

    feat (src/components/ui/InnerSpinner): Create InnerSpinner Component

---
+++ src/components/ui/InnerSpinner.jsx
@@ -0,0 +1,7 @@
+export default function InnerSpinner() {
+  return (
+    <div className="w-full h-full flex justify-center items-center my-8">
+      <div className="border-x-[#252c62] border-8 border-transparent w-12 h-12 rounded-full animate-spin"></div>
+    </div>
+  );
+}
