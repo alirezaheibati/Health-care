5156e5c9ca4c647f07109a71d49e7579e28f9202
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-02-06 18:55:45 +0330

    feat (src/components/shop/ItemsContainer): Integrate InnerSpinner component to indicate loading state

--- src/components/shop/ItemsContainer.jsx
+++ src/components/shop/ItemsContainer.jsx
@@ -1,13 +1,16 @@
 import ShopItem from "./ShopItem";
+import InnerSpinner from "../ui/InnerSpinner";
 import { useSelector } from "react-redux";
 
-export default function ItemsContainer() {
+export default function ItemsContainer({ isFetching }) {
   const items = useSelector((state) => state.items.items);
   return (
     <div className="p-4 flex justify-center sm:justify-between items-stretch flex-wrap gap-2 w-full md:w-2/3 lg:w-3/4 bg-green-500">
-      {items.map((item) => (
-        <ShopItem key={item.objectId} item={item} />
-      ))}
+      {isFetching ? (
+        <InnerSpinner />
+      ) : (
+        items.map((item) => <ShopItem key={item.objectId} item={item} />)
+      )}
     </div>
   );
 }
