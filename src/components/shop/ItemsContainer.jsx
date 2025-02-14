beae636b85f9ed7d9915f51f31fe2f7109343dda
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-02-14 12:22:09 +0330

    refactor (src/components/shop/ItemsContainer): Remove InnerSpinner component and related prop to it

--- src/components/shop/ItemsContainer.jsx
+++ src/components/shop/ItemsContainer.jsx
@@ -1,16 +1,13 @@
 import ShopItem from "./ShopItem";
-import InnerSpinner from "../ui/InnerSpinner";
 import { useSelector } from "react-redux";
 
-export default function ItemsContainer({ isFetching }) {
+export default function ItemsContainer() {
   const items = useSelector((state) => state.items.filterdItems);
   return (
-    <div className="p-4 flex justify-center sm:justify-between items-stretch flex-wrap gap-2 w-full md:w-2/3 lg:w-3/4 bg-green-500">
-      {isFetching ? (
-        <InnerSpinner />
-      ) : (
-        items.map((item) => <ShopItem key={item.objectId} item={item} />)
-      )}
+    <div className="p-4 md:pt-0 flex justify-center sm:justify-between items-stretch flex-wrap gap-2 w-full md:w-2/3 lg:w-3/4">
+      {items.map((item) => (
+        <ShopItem key={item.objectId} item={item} />
+      ))}
     </div>
   );
 }
