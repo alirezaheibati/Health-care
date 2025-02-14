d70f27baaeaef06d972463f3a7395e965e6cf397
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-02-14 12:15:40 +0330

    feat (src/components/ui/Spinner): Update Spinner component with new design

--- src/components/ui/Spinner.jsx
+++ src/components/ui/Spinner.jsx
@@ -5,8 +5,9 @@
  */
 export default function Spinner() {
   return (
-    <div className="relative w-full h-full p-8 flex justify-center items-center z-50">
-      <div className="border-x-orange-500 border-[12px] border-transparent w-40 h-40 rounded-full animate-spin"></div>
+    <div className="fixed left-0 right-0 top-0 w-screen h-screen p-8 flex justify-center items-center z-50">
+      <div className="absolute left-0 top-0 w-full h-full bg-black/50 z-[60]"></div>
+      <div className="border-x-orange-500 border-[12px] border-transparent w-40 h-40 rounded-full animate-spin z-[70]"></div>
     </div>
   );
 }
