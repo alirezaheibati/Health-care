b2bd2b418816a4753a2241f0413745057ffceb70
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-02-16 20:04:07 +0330

    refactor (src/components/Filter/FilterByBrands.jsx): use useEffect hook to initiate brands list

--- src/components/Filter/FilterByBrands.jsx
+++ src/components/Filter/FilterByBrands.jsx
@@ -1,11 +1,16 @@
 import { useDispatch, useSelector } from "react-redux";
+import { useMemo, useEffect } from "react";
 import { itemsActions } from "../../store/items-slice";
-
+let brandsList = [];
 export default function FilterByBrands() {
-  const filteredItems = useSelector((state) => state.items.items);
-  const brandsSet = new Set(filteredItems.map((item) => item.brand));
-  const brandsList = [...brandsSet];
   const dispatch = useDispatch();
+  const items = useSelector((state) => state.items.items);
+  const selectedBrand = useSelector((state) => state.items.brand);
+
+  useEffect(() => {
+    const brandsSet = new Set(items.map((item) => item.brand));
+    brandsList = [...brandsSet];
+  }, [brandsList]);
   function filterByBrandHandler(e) {
     dispatch(itemsActions.setBrand(e.target.value));
   }
@@ -22,6 +27,7 @@ export default function FilterByBrands() {
               id={item}
               className="mr-1 translate-y-[1px]"
               onChange={filterByBrandHandler}
+              checked={item === selectedBrand}
             />
             <label htmlFor={item}>{item}</label>
           </li>
