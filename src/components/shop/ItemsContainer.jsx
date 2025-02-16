16b3eb3b0d12c2baa21c9a996fdfd2adb54a7e23
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-02-16 20:13:22 +0330

    feat (src/components/shop/ItemsContainer): filter items according to redux state fields to render them on screen

--- src/components/shop/ItemsContainer.jsx
+++ src/components/shop/ItemsContainer.jsx
@@ -1,10 +1,40 @@
+import { itemsActions } from "../../store/items-slice";
 import ShopItem from "./ShopItem";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function ItemsContainer() {
-  const items = useSelector((state) => state.items.filterdItems);
+  const dispatch = useDispatch();
+  const {
+    items: allItems,
+    brand,
+    tag,
+    areInStock,
+    price,
+  } = useSelector((state) => state.items);
+
+  const items = allItems
+    .filter((item) => {
+      if (brand === "") return true;
+      else {
+        return brand === item.brand;
+      }
+    })
+    .filter((item) => {
+      if (tag === "") return true;
+      else {
+        return item.category.includes(tag);
+      }
+    })
+    .filter((item) => {
+      if (!areInStock) return true;
+      else {
+        return item.amount > 0;
+      }
+    })
+    .filter((item) => item.price >= price.min && item.price <= price.max);
+  dispatch(itemsActions.setFilteredItemsCount(items.length));
   return (
-    <div className="p-4 md:pt-0 flex justify-center sm:justify-between items-stretch flex-wrap gap-2 w-full md:w-2/3 lg:w-3/4">
+    <div className="p-4 md:pt-0 flex justify-center sm:justify-start items-stretch flex-wrap gap-2 sm:gap-[1%] lg:gap-[2%%] xl:gap-[1.33%] w-full md:w-2/3 lg:w-3/4">
       {items.map((item) => (
         <ShopItem key={item.objectId} item={item} />
       ))}
