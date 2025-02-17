9fbb8f134769f77ca612ba9e64eb6aa912a8fe25
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-02-17 17:34:06 +0330

    feat (src/components/Filter/FilterModal): add clear filters button to run reset reducer on click

--- src/components/Filter/FilterModal.jsx
+++ src/components/Filter/FilterModal.jsx
@@ -4,7 +4,6 @@ import { itemsActions } from "../../store/items-slice";
 import FilterByStock from "./FilterByStock";
 import FilterByBrands from "./FilterByBrands";
 import FilterByTags from "./FilterByTags";
-
 export default function FilterModal() {
   const dispatch = useDispatch();
   const filtersVisibility = useSelector(
@@ -16,6 +15,9 @@ export default function FilterModal() {
   function hideFiltersWindow() {
     dispatch(itemsActions.toggleFilterModal());
   }
+  function handleClearFilter() {
+    dispatch(itemsActions.resetFilters());
+  }
   return (
     <div
       className={`bg-white flex flex-col justify-between fixed md:relative w-screen h-screen top-0 left-0 md:block md:w-1/3 lg:w-1/4 p-3 pr-6 md:pr-3 ${
@@ -25,7 +27,12 @@ export default function FilterModal() {
       } transition-transform`}
     >
       <div className="grow overflow-y-auto">
-        <h2 className="font-semibold text-2xl">Filters</h2>
+        <div className="flex justify-between items-center">
+          <h2 className="font-semibold text-2xl">Filters</h2>
+          <button className="text-red-500" onClick={handleClearFilter}>
+            Clear Filters
+          </button>
+        </div>
         <hr className="my-2" />
         <FilterByStock />
         <hr className="my-2" />
