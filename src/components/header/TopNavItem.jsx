c9819df5ab114286311b8e06321e593c64a29218
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-02-17 17:27:34 +0330

    feat (src/components/header/TopNavItem): use NavLink in TopNavItem component to specify active link

--- src/components/header/TopNavItem.jsx
+++ src/components/header/TopNavItem.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 /**
  * TopNavItem Component
  *
@@ -16,12 +16,23 @@ import { Link } from "react-router-dom";
  */
 export default function TopNavItem({ title, icon, isSpecial, linkTo }) {
   return (
-    <li className={`group ${isSpecial ? "text-orange-500" : "text-slate-700"}`}>
-      <div className="px-3">
-        <FontAwesomeIcon icon={icon} className="text-sm mb-[2px] mr-1" />
-        <Link to={linkTo}>{title}</Link>
-      </div>
-      <div className="w-0 bg-slate-700 h-[2px] group-hover:w-full transition-width duration-300 ease-in-out"></div>
+    <li
+      className={`group px-3 ${
+        isSpecial ? "text-orange-500" : "text-slate-700"
+      }`}
+    >
+      <NavLink
+        to={linkTo}
+        end={true}
+        className={({ isActive }) => (isActive ? "[&_div]:w-full" : "")}
+      >
+        <p>
+          <FontAwesomeIcon icon={icon} className="text-sm mb-[2px] mr-1" />
+          {title}
+        </p>
+
+        <div className="w-0 bg-slate-700 h-[2px] group-hover:w-full transition-width duration-300 ease-in-out"></div>
+      </NavLink>
     </li>
   );
 }
