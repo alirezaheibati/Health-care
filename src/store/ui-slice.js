2707ff11f439b6ab232e171a3aed8eece5a66e53
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-02-25 09:31:00 +0330

    feat (src/store/ui-slice.js): Add showCart and hideCart reducers to manage cart visibility state

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -2,13 +2,19 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
   name: "ui",
-  initialState: { spinnerVisibility: false },
+  initialState: { spinnerVisibility: false, cartVisibility: false },
   reducers: {
     showSpinner(state) {
       state.spinnerVisibility = true;
     },
     hideSpinner(state) {
       state.spinnerVisibility = false;
+    },
+    showCart(state) {
+      state.cartVisibility = true;
+    },
+    hideCart(state) {
+      state.cartVisibility = false;
     },
   },
 });
