01fe62085ad1e834b2dd0409354945ae6cb771cc
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-02-27 19:47:51 +0330

    style (tailwind.config.js): Extend Tailwind CSS with custom box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "qbox-shadow": "rgba(99, 99, 99, 0.2) 0px 1px 1px 1px",
+      },
+    },
     screens: {
       sm: "480px",
       md: "768px",
