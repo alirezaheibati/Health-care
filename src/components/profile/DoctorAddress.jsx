c4f3a618be5e072cfd234cd623d9968d87d2ad1c
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-02-28 22:33:43 +0330

    feat (src/components/profile): update DoctorAddress component to show office location on Leaflet map

--- src/components/profile/DoctorAddress.jsx
+++ src/components/profile/DoctorAddress.jsx
@@ -1,7 +1,14 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationDot, faSignsPost } from "@fortawesome/free-solid-svg-icons";
-
-export default function DoctorAddress({ doctor, address }) {
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useEffect, useRef } from "react";
+export default function DoctorAddress({ doctor, address, coordinates }) {
+  const markerRef = useRef(null);
+  useEffect(() => {
+    if (markerRef.current) {
+      markerRef.current.openPopup();
+    }
+  }, [markerRef.current]);
   return (
     <div className="bg-gray-100 p-2 sm:p-4 mt-3 rounded-xl">
       <h2 className="text-xl mb-2 ">Address of {doctor}'s office</h2>
@@ -9,10 +16,21 @@ export default function DoctorAddress({ doctor, address }) {
         <FontAwesomeIcon icon={faLocationDot} className="mr-1" />
         {address}
       </address>
-      <button className="text-sky-600 mt-4">
-        <FontAwesomeIcon icon={faSignsPost} className="mr-1" />
-        Show on map
-      </button>
+      <div id="map">
+        {doctor && (
+          <MapContainer center={coordinates} zoom={13} className="h-60 mt-4">
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={coordinates} ref={markerRef}>
+              <Popup closeButton={false} closeOnEscapeKey={false}>
+                {doctor}'s office: {address}
+              </Popup>
+            </Marker>
+          </MapContainer>
+        )}
+      </div>
     </div>
   );
 }
