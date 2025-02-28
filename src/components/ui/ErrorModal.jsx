b963e98f1f3f298d9abf890f63d0851b923eb929
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-02-28 22:48:53 +0330

    feat (src/components/ui/ErrorModal): update ErrorModal to display error modal with title and message from the Redux state

--- src/components/ui/ErrorModal.jsx
+++ src/components/ui/ErrorModal.jsx
@@ -1,3 +1,6 @@
+import { useDispatch, useSelector } from "react-redux";
+import { uiActions } from "../../store/ui-slice";
+
 /**
  * ErrorModal Component
  *
@@ -7,13 +10,21 @@
  * <ErrorModal />
  */
 export default function ErrorModal() {
+  const error = useSelector((state) => state.ui.errorModal);
+  const dispatch = useDispatch();
+  function closeModalHandler() {
+    dispatch(uiActions.hideErrorModal());
+  }
   return (
-    <div className="fixed left-0 top-0 w-screen h-screen flex justify-center items-center z-[100]">
+    <div className="fixed left-0 top-0 w-screen h-screen flex justify-center items-center z-[10000]">
       <div className="absolute top-0 left-0 bg-black/50 w-full h-full"></div>
       <div className=" bg-slate-50 rounded-xl px-4 w-[400px] max-w-full relative">
-        <p className="font-bold text-center my-4">{errorInfo.title}</p>
-        <p>{errorInfo.message}</p>
-        <button className="block mx-auto bg-slate-900 text-slate-50 rounded-lg py-1 px-8 my-4">
+        <p className="font-bold text-center my-4">{error.title}</p>
+        <p>{error.message}</p>
+        <button
+          className="block mx-auto bg-slate-900 text-slate-50 rounded-lg py-1 px-8 my-4"
+          onClick={closeModalHandler}
+        >
           Ok
         </button>
       </div>
