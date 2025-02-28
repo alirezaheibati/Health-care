289fa6a3e890b3809a6e604fc64dfbd5a8896426
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-02-28 22:38:06 +0330

    feat (src/store/business-card-slice): create business-cards-slice for managing business cards state and actions

---
+++ src/store/business-card-slice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const businessCardsSlice = createSlice({
+  name: "cards",
+  initialState: {
+    cards: [],
+    filterClause: "All Experties",
+  },
+  reducers: {
+    setCards(state, action) {
+      state.cards = [...action.payload];
+    },
+    sortCards(state, action) {
+      if (action.payload === "price-asc") {
+        state.cards.sort((a, b) => b.fee - a.fee);
+      } else if (action.payload === "price-desc") {
+        state.cards.sort((a, b) => a.fee - b.fee);
+      } else if (action.payload === "examined") {
+        state.cards.sort((a, b) => b.patients - a.patients);
+      } else {
+        state.cards.sort((a, b) => b.rating - a.rating);
+      }
+    },
+    setFilterClause(state, action) {
+      state.filterClause = action.payload;
+    },
+  },
+});
+export const cardsActions = businessCardsSlice.actions;
+export default businessCardsSlice;
