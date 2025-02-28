812a1dfa661d79ddffb790a7c9bd60b7d134d705
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-02-28 22:39:01 +0330

    feat (src/store/index.js): integrate businessCardsSlice into Redux store

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import uiSlice from "./ui-slice";
 import itemsSlice from "./items-slice";
 import cartSlice from "./cart-slice";
+import businessCardsSlice from "./business-card-slice";
 const store = configureStore({
   reducer: {
     ui: uiSlice.reducer,
     items: itemsSlice.reducer,
     cart: cartSlice.reducer,
+    cards: businessCardsSlice.reducer,
   },
 });
 export default store;
