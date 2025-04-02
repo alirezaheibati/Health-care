b7dcbdc8c2cc3040f50881fe337e7676d38ebd91
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-04-02 15:56:17 +0330

    feat (src/components/doctors/ExpertiseBtn): Add onClick functionality for selecting expertise with dynamic styles

--- src/components/doctors/ExpertiseBtn.jsx
+++ src/components/doctors/ExpertiseBtn.jsx
@@ -1,3 +1,6 @@
+import { useDispatch, useSelector } from "react-redux";
+import { cardsActions } from "../../store/business-card-slice";
+
 /**
  * ExpertiseBtn Component
  *
@@ -8,12 +11,20 @@
  * - `image` (string): The URL of the image representing the expertise category.
  * - `isActive` (bool): Indicates whether the button is active or not.
  */
-export default function ExpertiseBtn({ title, image, isActive }) {
+export default function ExpertiseBtn({ title, image }) {
+  const activeExperty = useSelector((state) => state.cards.experty);
+  const dispatch = useDispatch();
+  function expertiesSelectHandler() {
+    dispatch(cardsActions.setExperty(title));
+  }
   return (
     <li
       className={`${
-        isActive ? "text-slate-100 bg-[#252c62]" : "bg-slate-100 text-[#252c62]"
+        activeExperty === title
+          ? "text-slate-100 bg-[#252c62]"
+          : "bg-slate-100 text-[#252c62]"
       } rounded-full shrink-0`}
+      onClick={expertiesSelectHandler}
     >
       <button className="flex justify-center items-center gap-2 px-4 py-2">
         <p>{title}</p>
