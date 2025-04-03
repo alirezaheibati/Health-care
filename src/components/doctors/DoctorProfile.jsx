3649e34d7841f1dce1af54a884028e1628031e96
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-04-03 10:53:01 +0330

    feat (src/components/doctors/DoctorProfile): Use Link component for navigation to doctors profile page

--- src/components/doctors/DoctorProfile.jsx
+++ src/components/doctors/DoctorProfile.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 /**
  * DoctorProfile Component
  *
@@ -8,16 +10,18 @@
  * - `image` (string): The URL of the image representing the doctor.
  * - `expertise` (string): The doctor's area of expertise.
  */
-export default function DoctorProfile({ name, image, expertise }) {
+export default function DoctorProfile({ card }) {
   return (
     <li className="w-[49%] md:w-[33%] lg:w-[19%] shrink-0">
-      <div className=" bg-gray-200 rounded-3xl overflow-hidden">
-        <img src={image} alt={name} />
-      </div>
-      <h2 className="text-[#252c62] text-lg sm:text-xl font-semibold pt-2">
-        {name}
-      </h2>
-      <p className="text-gray-500">{expertise}</p>
+      <Link to={`/projects/healthcare/book-appointment/${card.objectId}`}>
+        <div className=" bg-gray-200 rounded-3xl overflow-hidden">
+          <img src={card.image} alt={card.username} />
+        </div>
+        <h2 className="text-[#252c62] text-lg sm:text-xl font-semibold pt-2">
+          {card.username}
+        </h2>
+        <p className="text-gray-500">{card.expertise}</p>
+      </Link>
     </li>
   );
 }
